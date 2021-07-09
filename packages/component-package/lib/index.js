dff1374c7ccc9407c8492126841bc077555b8efb
xujianzhen <xujianzhen@dxy.cn>
2021-07-09 10:26:02 +0800

    发布测试

--- packages/component-package/lib/index.js
+++ packages/component-package/lib/index.js
@@ -5,7 +5,7 @@ const { util } = require("util-package");
 
 module.exports = {
   component: function component() {
-    console.log("我是 component package1");
+    console.log("我是 component package157657657576");
     console.log("下面将从 component package 中执行 utils packages 中的方法");
     util();
   }
--- packages/utils-package/lib/index.js
+++ packages/utils-package/lib/index.js
@@ -1,6 +1,6 @@
 
 module.exports = {
   util: function util () {
-    console.log('我是 util 函数 变更啦')
+    console.log('我是 util 函数 变更啦222222')
   }
 }
